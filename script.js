5f7e2ae72d8a4a589f1a13f6ff281d62d52fa229
IwonaMaurer <plastimex2@wp.pl>
2017-12-17 14:23:42 +0100

    zadanie

---
+++ script.js
@@ -0,0 +1,159 @@
+
+/*
+class Stopwatch {
+    constructor(display) {
+        this.running = false;
+        this.display = display;
+        this.reset();
+        this.print(this.times);
+    }
+
+    reset() {
+        this.times = {
+            minutes: 0,
+            seconds: 0,
+            miliseconds: 0
+        };
+
+}
+    print() {
+        this.display.innerText = this.format(this.times);
+    }
+    
+    format(times) {
+        return `${pad0(times.minutes)}:${pad0(times.seconds)}:${pad0(Math.floor(times.miliseconds))}`;
+    }
+    start() {
+        if (!this.running) {
+            this.running = true;
+            this.watch = setInterval(() => this.step(), 10);
+        }
+    }
+    
+    step() {
+        if (!this.running) return;
+        this.calculate();
+        this.print();
+    }
+    
+    calculate() {
+        this.times.miliseconds += 1;
+        if (this.times.miliseconds >= 100) {
+            this.times.seconds += 1;
+            this.times.miliseconds = 0;
+        }
+        if (this.times.seconds >= 60) {
+            this.times.minutes += 1;
+            this.times.seconds = 0;
+        }
+    }
+    
+    stop() {
+        this.running = false;
+        clearInterval(this.watch);
+    }
+}
+
+
+function pad0(value) {
+    let result = value.toString();
+    if (result.length < 2) {
+        result = '0' + result;
+    }
+    return result;
+}
+
+
+
+const stopwatch = new Stopwatch(
+document.querySelector('.stopwatch'));
+
+var startButton = document.getElementById('start');
+startButton.addEventListener('click', () => stopwatch.start());
+
+var stopButton = document.getElementById('stop');
+stopButton.addEventListener('click', () => stopwatch.stop());
+
+
+
+var app = React.createElement(App);
+ReactDOM.render(app, document.getElementById('app'));
+
+
+
+*/
+
+
+class Stopwatch extends React.Component {
+    constructor() {
+        super ();
+        this.running = false;
+        this.reset();
+    }
+
+    reset() {
+        this.state = {
+                times: {
+                    minutes: 0,
+                    seconds: 0,
+                    miliseconds: 0
+                }
+        };
+    }
+
+    format (times) {
+        return `${pad0(times.minutes)}:${pad0(times.seconds)}:${pad0(Math.floor(times.miliseconds))}`;
+    }
+    start() {
+        if (!this.running) {
+            this.running = true;
+            this.watch = setInterval(() => this.step(), 10);
+        }
+    }
+    step() {
+        if (!this.running) return;
+        this.calculate();
+    }
+    calculate() {
+        let newTimes = {
+            miliseconds: this.state.times.miliseconds,
+            seconds: this.state.times.seconds,
+            minutes: this.state.times.minutes
+        };
+        newTimes.miliseconds += 1;
+
+        if (newTimes.miliseconds >=100) {
+            newTimes.seconds += 1;
+            newTimes.miliseconds = 0;
+        }
+        if (newTimes.seconds >= 60) {
+            newTimes.minutes += 1;
+            newTimes.seconds =0;
+        }
+        this.setState({times: newTimes});
+    }
+	stop() {
+		this.running = false;
+		clearInterval(this.watch);
+	}
+	render () {
+        return (
+            <div>
+                <nav>
+                    <button onClick={(e) => this.start(e)}>Start</button>
+                    <button onClick={(e) => this.stop(e)}>Stop</button>
+                </nav>
+                {this.format(this.state.times)}
+            </div>
+        );
+    }
+}
+function pad0(value) {
+    let result = value.toString();
+    if (result.length < 2) {
+        result = '0' + result;
+    }
+    return result;
+}
+
+ReactDOM.render(<Stopwatch/>, document.getElementById('app'));
